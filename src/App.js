5527847514c6047a24ce78327fb8ca53d0e84580
Thiago Lanza <thlanza@hotmail.com>
2023-04-16 17:44:57 -0300

    urls inválidas

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   const getUser = async () => {
     try {
-      const url = `${process.env.REACT_APP_API_URL}/api/alunos/login/success`;
+      const url = `${process.env.REACT_APP_API_URL}/auth/login/success`;
       const { data } = await axios.get(url, { withCredentials: true });
       setUser(data.user._json)
     } catch (err) {
