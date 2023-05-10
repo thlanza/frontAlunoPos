11032f532875148397328d7e972b51ac511e53d9
Thiago Lanza <thlanza@hotmail.com>
2023-05-10 20:20:23 -0300

    url

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://backend-da-academia-lanza-modulo-3-da-pos.onrender.com/api";
-export const baseUrl = "http://localhost:5000/api";
+export const baseUrl = "https://backend-da-academia-lanza-modulo-3-da-pos.onrender.com/api";
+// export const baseUrl = "http://localhost:5000/api";
